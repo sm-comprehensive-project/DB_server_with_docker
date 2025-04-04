69c85e0c43cc971e6a943c4f3eee768ed18a98f8
Yeonghak Yun <yhoka@naver.com>
2025-04-04 18:29:54 +0900

    4/4 elastic search test success & complete mongodb replicaset setting

--- test/mongodb/replicaSet.js
+++ test/mongodb/replicaSet.js
@@ -1,9 +1,9 @@
 config = {
   _id: "replication",
   members: [
-    { _id: 0, host: "mongo1:27017", priority: 1 }, 
-    { _id: 1, host: "mongo2:27017", priority: 0 }, 
-    { _id: 2, host: "mongo3:27017", priority: 0 }  
+    { _id: 0, host: "test-mongo1-1:27017", priority: 3 }, 
+    { _id: 1, host: "test-mongo2-1:27017", priority: 2 }, 
+    { _id: 2, host: "test-mongo3-1:27017", priority: 1 }  
   ]
 }
 
